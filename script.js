a234cbf527f7cc4a9b4a23ffe59e9a22a73fa8aa
Adarsh Kishore <adarsh.kishore786@gmail.com>
2023-10-18 00:47:59 +0530

    Image and text change

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ function checkHasWon() {
     "<br>You've got the message- now find out if you've done it best: " + 
     "<br> 1. Screenshot this page" +
     "<br> 2. Tag @istenitk" +
-    "<br> 3. Post it on your Instagram story";
+    "<br> 3. Post it on your Instagram story to get featured by us.";
 }
 
 let imgOrder = shuffle(original);
